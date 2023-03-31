da1ba2abdb4d8f4f17e1686dc79da58f03dd8eb3
Daiane Procópio <100447878+procopiodaiane@users.noreply.github.com>
2023-03-31 19:10:43 -0300

    Delete MelhorIdade.sql

--- src/MelhorIdade.sql
+++
@@ -1,9 +0,0 @@
-USE Melhoridade;
-
-SELECT * FROM Usuario;
-DELETE FROM Usuario WHERE Codigo = 1;
-SELECT * FROM Usuario;
-
-SELECT * FROM checkup;
-UPDATE checkup SET DataProximo = '2002/03/24' WHERE Codigo = '4';
-SELECT * FROM checkup;
